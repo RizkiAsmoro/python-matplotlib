142b818de63c8597da9ceaa7eeef97817272ae5f
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-05-21 14:12:01 +0700

    adding grid

--- labels.py
+++ labels.py
@@ -13,4 +13,12 @@ pl.plot(x,y, marker = 'o', mfc = 'red')
 pl.title("This is title", loc = 'left') # loc for positioning the title, the dafault is center
 pl.xlabel("lebel x", c = 'red') #another attribute can be added
 pl.ylabel("label y", c = 'yellow') #another attribute can be added
+pl.grid() #adding grid (grid can be specify line x or y)
+pl.show()
+
+pl.plot(x,y, marker = 'o', mfc = 'red')
+pl.title("This is title", loc = 'left') # loc for positioning the title, the dafault is center
+pl.xlabel("lebel x", c = 'red') #another attribute can be added
+pl.ylabel("label y", c = 'yellow') #another attribute can be added
+pl.grid(axis= 'y') #adding grid (grid can be specify line x or y)
 pl.show()
