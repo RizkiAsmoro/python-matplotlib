4a2d660dcff31dd53da2ed3ae687bc6c9cf90fc3
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-05-24 12:55:19 +0700

    alpha, color

--- scatter.py
+++ scatter.py
@@ -14,11 +14,13 @@ import numpy as np
 #day1
 xAge = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
 ySpeed = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-plt.scatter(xAge,ySpeed)
+plt.scatter(xAge,ySpeed, alpha=0.5) #alpha for adjust transparancy
 
 #day2
 xAge = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
 ySpeed = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
-plt.scatter(xAge, ySpeed)
+plt.scatter(xAge, ySpeed,color = '#88c999')
 
+
+plt.colorbar()
 plt.show()
