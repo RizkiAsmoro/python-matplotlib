ac1f1b6a1b50f559e89afd70decf2826d047b98e
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-06-10 13:45:59 +0700

    coloring

--- pie.py
+++ pie.py
@@ -7,6 +7,7 @@ import numpy as np
 x = np.array([35,15,20,20,10])
 xlabels = ["ronaldo","neymar","messi","mbape","haland"]
 xexplode = [0.2,0.1,0,0,0] #explode one of wedges from the center 
+xcolors = ['pink','b','r','g','y'] #coloring the wedges
 
-pl.pie(x, labels = xlabels, explode = xexplode)
+pl.pie(x, labels = xlabels, explode = xexplode, colors=xcolors)
 pl.show()
