c9892f8b5cbded92f8f2fe79a1d4ed5c188a28fe
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-06-10 13:41:25 +0700

    explode wedges

--- pie.py
+++ pie.py
@@ -6,6 +6,7 @@ import numpy as np
 
 x = np.array([35,15,20,20,10])
 xlabels = ["ronaldo","neymar","messi","mbape","haland"]
+xexplode = [0.2,0.1,0,0,0] #explode one of wedges from the center 
 
-pl.pie(x, labels = xlabels )
+pl.pie(x, labels = xlabels, explode = xexplode)
 pl.show()
