42006b75870775e6f3866f502984eebe6121a30f
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-06-10 14:16:08 +0700

    list explanation

--- pie.py
+++ pie.py
@@ -10,4 +10,5 @@ xexplode = [0.2,0.1,0,0,0] #explode one of wedges from the center
 xcolors = ['pink','b','r','g','y'] #coloring the wedges
 
 pl.pie(x, labels = xlabels, explode = xexplode, colors=xcolors)
+pl.legend(title="Players") #add list explanation
 pl.show()
